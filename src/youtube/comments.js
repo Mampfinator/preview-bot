f0a6e9b51d2cfdf7cdddc4290f8ce05308da1050
Sir Eatsalot <46071499+Mampfinator@users.noreply.github.com>
2025-08-10 11:27:14 +0200

    YouTube Comments (#16)
    * initial comment preview implementation
    
    * full implementation of comment previews
    
    * upgrade to youtube.js master

---
+++ src/youtube/comments.js
@@ -0,0 +1,184 @@
+const { EmbedBuilder } = require("@discordjs/builders");
+const { ScrapingClient, YouTubeClient } = require("@sireatsalot/youtube.js");
+
+// yes this is excessive.
+const REGEX = /https:\/\/(www\.)?youtube\.com\/(post|shorts)\/[^ ]+?lc=[A-Za-z0-9-_]+|https:\/\/(www\.)?youtube\.com\/watch\?v=[A-Za-z0-9-_]+&lc=[A-Za-z0-9-_]+/g;
+
+class YouTubeVideoCommentsPreviewGenerator {
+    /**
+     * @type {ScrapingClient} client
+     */
+    scrapingClient;
+
+    /**
+     * 
+     * @param {APIClient} client 
+     */
+    
+    constructor(client) {
+        this.scrapingClient = client;
+        this.apiClient = new YouTubeClient({key: process.env.YOUTUBE_API_KEY});
+    }
+
+    async generate(match) {
+        if (!match.includes("watch")) return null;
+
+        const comments = await this.apiClient.comments.list({
+            part: ["snippet", "id"],
+            id: match.split("lc=")[1],
+        });
+
+        if (comments.isErr()) {
+            throw comments.error;
+        }
+
+        const comment = comments.value[0];
+
+        if (!comment) return null;
+        return {
+            message: {
+                embeds: [
+                    apiCommentToEmbed(comment)
+                ]
+            }
+        }
+    }
+}
+
+function apiCommentToEmbed(comment) {
+    const { snippet } = comment;
+    return new EmbedBuilder()
+        .setDescription(snippet.textOriginal)
+        .setAuthor({
+            name: `Comment by ${snippet.authorDisplayName}`,
+            iconURL: snippet.authorProfileImageUrl,
+            url: `https://www.youtube.com/channel/${snippet.authorChannelId.value}`,
+        })
+        .setColor(0xFF0000)
+        .setFooter({
+            text: "ID: " + comment.id,
+            iconURL: "https://www.youtube.com/img/favicon_144.png"
+        });
+}
+
+class YouTubeShortsCommentsPreviewGenerator {
+    /**
+     * @type {ScrapingClient} client
+     */
+    client;
+
+    constructor(client) {
+        this.client = client;
+    }
+
+
+    async generate(match) {
+        if (!match.includes("shorts")) return null;
+
+        const url = new URL(match);
+
+        const videoId = url.pathname.split("/").pop();
+        const commentId = url.searchParams.get("lc");
+
+        if (!videoId || !commentId) return null;
+
+        const result = await this.client.short(videoId, commentId)
+            .fetchComments();
+
+        const comments = result._unsafeUnwrap();
+
+        const comment = await comments.fetchHighlightedComment();
+
+        if (!comment) return null;
+
+        return {
+            message: {
+                embeds: [
+                    commentToEmbed(comment)
+                ]
+            }
+        };
+    }
+}
+
+class YouTubePostCommentsPreviewGenerator {
+    /**
+     * @type {ScrapingClient} client
+     */
+    client;
+
+    constructor(client) {
+        this.client = client;
+    }
+
+    async generate(match) {
+        if (!match.includes("post")) return null;
+
+        const url = new URL(match);
+
+        const postId = url.pathname.split("/").pop();
+        const commentId = url.searchParams.get("lc");
+
+        if (!postId || !commentId) return null;
+
+        const result = await this.client.post(postId, commentId)
+            .fetchComments();
+
+        const comments = result._unsafeUnwrap();
+
+        const comment = await comments.fetchHighlightedComment();
+
+        if (!comment) return null;
+
+        return {
+            message: {
+                embeds: [
+                    commentToEmbed(comment)
+                ]
+            }
+        };
+    }
+}
+
+function commentToEmbed(comment) {
+    return new EmbedBuilder()
+        .setAuthor({
+            name: `Comment by ${comment.author.name}`,
+            iconURL: comment.author.avatar,
+            url: `https://www.youtube.com/channel/${comment.author.id}`,
+        })
+        .setDescription(comment.content)
+        .setColor(0xFF0000)
+        .setFooter({
+            text: "ID: " + comment.id,
+            iconURL: "https://www.youtube.com/img/favicon_144.png"
+        })
+}
+
+class YouTubeCommentPreview {
+    client;
+    
+    constructor(
+        client
+    ) {
+        this.client = client;
+        this.generators = [
+            new YouTubeVideoCommentsPreviewGenerator(client),
+            new YouTubeShortsCommentsPreviewGenerator(client),
+            new YouTubePostCommentsPreviewGenerator(client)
+        ]
+    }
+    
+    match(content) {
+        const matches = [...content.matchAll(REGEX)].map(m => typeof m === "string" ? m : m[0]);
+        return matches;
+    }
+
+    async init() {
+        await this.client.init();
+    }
+}
+
+module.exports = {
+    YouTubeCommentPreview
+}
---
+++ src/youtube/community-posts.js
@@ -0,0 +1,146 @@
+const { ScrapingClient, AttachmentType } = require("@sireatsalot/youtube.js");
+const { EmbedBuilder } = require("discord.js");
+const { unwrap } = require("../util");
+const { Cache } = require("../cache");
+
+const postIdRegex =
+    /(?<=youtube.com\/post\/)Ug[A-z0-9_\-]+|(?<=youtube.com\/channel\/.+\/community\?lb=)Ug[A-z0-9_\-]+/g;
+
+class CommunityPostPreviewGenerator {
+    #client = new ScrapingClient();
+
+    #cache = new Cache();
+
+    /**
+     * @param {string} match
+     */
+    async generate(match) {
+        const postScraper = this.#client.post(match);
+
+        /**
+         * @type { import("@sireatsalot/youtube.js").CommunityPost }
+         */
+        const post = await postScraper.getPost().then(unwrap);
+        const channel = await postScraper.getChannelData().then(unwrap);
+
+        this.#cache.set(match, post);
+
+        try {
+            return {
+                message: {
+                    embeds: [
+                        postToEmbed(post, channel)
+                    ],
+                },
+                images: post.images?.length ?? 0,
+            }
+        } catch (error) {
+            console.error(error);
+            return null;
+        }
+    }
+
+    async getImage(id, imageNo) {
+        const post = await this.#client.post(id).getPost(true).then(result => result._unsafeUnwrap());
+
+        return {
+            image: post.images?.[imageNo],
+            totalImages: post.images?.length ?? 0,
+        }
+    }
+
+    async init() {
+        await this.#client.init();
+    }
+}
+
+/**
+ * Converts a Community Post into an {@link EmbedBuilder}.
+ * @param { import("@sireatsalot/youtube.js").CommunityPost } post
+ * @param { import("@sireatsalot/youtube.js").ChannelData } channel
+ */
+function postToEmbed(post, channel) {
+    const { content, attachmentType, id: postId } = post;
+
+    const { avatar, name, id: channelId } = channel;
+
+    const embed = new EmbedBuilder().setAuthor({
+        name,
+        iconURL: avatar,
+        url: `https://www.youtube.com/channel/${channelId}`,
+    });
+
+    let embedContent;
+
+    if (content && content.length > 0)
+        embedContent = content
+            .map(({ text, url }) => `${url ? "[" : ""}${text}${url ? `](${url})` : ""}`)
+            .join(" ");
+
+    embed
+        .setURL(`https://youtube.com/post/${postId}`)
+        .setColor("#ff0000")
+        .setFooter({
+            iconURL: "https://www.youtube.com/img/favicon_144.png",
+            text: `ID: ${postId}`,
+        });
+
+    switch (attachmentType) {
+        case AttachmentType.None:
+            break;
+        case AttachmentType.Image:
+            embed.setImage(post.images[0]);
+            break;
+        case AttachmentType.Video:
+            const { video } = post;
+            embed.addFields({
+                name: "Video",
+                value: `${video.title}\n[Click here](https://youtube.com/watch?v=${video.id})`,
+            });
+            embed.setImage(video.thumbnail);
+            break;
+        case AttachmentType.Playlist:
+            const { playlist } = post;
+            embedContent += `\n\nPlaylist: ${playlist.title} [link](https://youtube.com/playlist?list=${playlist.id})`;
+            embed.setImage(playlist.thumbail);
+            break;
+        case AttachmentType.Poll:
+        case AttachmentType.Quiz:
+            const { choices } = post;
+            embedContent += "\n\u200b\n\u200b";
+            embed.addFields({
+                name: "Poll",
+                value: choices.map(choice => `\u2022 \u200b ${choice.text}`).join("\n"),
+            });
+            break;
+    }
+
+    if (embedContent && embedContent.length > 0) embed.setDescription(embedContent);
+
+    return embed;
+}
+
+class YouTubeCommunityPostPreview {
+    name = "youtube-community-posts";
+
+    constructor(client) {
+        this.generators = [
+            new CommunityPostPreviewGenerator(client)
+        ]
+    }
+
+    /**
+    * @returns { string[] }
+    */
+    match(content) {
+        return [...content.matchAll(postIdRegex)].map(match => typeof match == "string" ? match : match[0]);
+    }
+    getImage(id, imageNo) {
+        return this.generators[0].getImage(id, imageNo);
+    }
+}
+
+module.exports = {
+    YouTubeCommunityPostPreview,
+    postToEmbed,
+}
